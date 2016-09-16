3ac58ebc008f0f66585298ba9c7cf7d0f6628629
Vitalik Stetsenko <vitalikstetsenko@hotmail.com>
2016-09-16 15:24:45 +0300

    add subscriber to admin & remove 'Email' label

---
+++ app/admin/subscriber.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register Subscriber do
+  permit_params :email
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :created_at
+    actions
+  end
+
+  filter :email
+
+  form do |f|
+    f.inputs "Subscriber Details" do
+      f.input :email
+    end
+    f.actions
+  end
+
+end

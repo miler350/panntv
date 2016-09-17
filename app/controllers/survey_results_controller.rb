2ff652a57bd9ece540df5c9aa6de78590b937c15
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-09-17 08:15:02 -0400

    fixed typos in survey results controller

--- app/controllers/survey_results_controller.rb
+++ app/controllers/survey_results_controller.rb
@@ -1,9 +1,9 @@
 class SurveyResultsController < ApplicationController
 
   def create
-    @survay = SurveyResult.new(survay_params)
+    @survay = SurveyResult.new(survey_params)
     if @survay.save
-      flash[:success] = "Thank you for Survay!"
+      flash[:success] = "Thank you. Your survey was submitted successfully."
     else
       flash[:errors] = "#{ @subscriber.errors.full_messages.join(', ') }"
     end
@@ -11,7 +11,7 @@ class SurveyResultsController < ApplicationController
   end
 
   private
-    def survay_params
+    def survey_params
       params.require(:survey_result).permit(:gender, :age, :income, :did_you_watch, :are_you_black, :favorite_programs,
         :favorite_programs_1, :favorite_programs_2, :first_visited, :news_network, :location, :suggest_events,
         :to_cover, :mostly_use, :did_you_hear, :like_to_see, :things_like, :things_dislike, :first_visited_other,
